9da0025859c8ec924d4bfad3808ff85ce9115c57
alejorom <morojela-github@yahoo.com>
2022-01-13 16:58:46 -0500

    Usuarios/ngBootstrap/SSL

--- API/Startup.cs
+++ API/Startup.cs
@@ -48,7 +48,7 @@ namespace API
 
             app.UseRouting();
 
-            app.UseCors(p => p.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+            app.UseCors(p => p.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
