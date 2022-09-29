80b46bd66e71a2fbb4c246ff7f31798c254f846d
alejorom <94083673+alejorom@users.noreply.github.com>
2022-09-29 16:33:32 -0500

    Agregar metodo login

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -37,6 +37,25 @@ namespace API.Controllers
             return user;
         }
 
+        [HttpPost("login")]
+        public async Task<ActionResult<AppUser>> Login (LoginDto loginDto)
+        {
+            var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName.ToLower() == loginDto.Username.ToLower());
+
+            if (user == null) return Unauthorized("Invalid user name or password");
+
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+            var computeHash = hmac.ComputeHash(Encoding.UTF8.GetBytes (loginDto.Password));
+
+            for (int i = 0; i < computeHash.Length; i++)
+            {
+                if (computeHash[i] != user.PasswordHash[i]) return Unauthorized ("Invalid user name or password");
+            }
+
+            return user;
+
+        }
+
         private async Task<bool> UserExists(string userName)
         {
             return await _context.Users.AnyAsync( x => x.UserName == userName.ToLower() );
